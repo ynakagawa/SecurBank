f2753ae67bf02bfcc59ecf9600383a15ed320a84
ynaka-adobe-com <ynaka@adobe.com>
2025-05-14 15:59:17 -0700

    add access-control header

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,21 @@ root.render(
   </React.StrictMode>
 );
 
+App.use((req, res, next) => {
+  // Set the Access-Control-Allow-Origin header to allow requests from your frontend's origin
+  res.setHeader('Access-Control-Allow-Origin', 'https://securbankdemo.vercel.app');
+
+  // Optionally, you can also allow other headers and methods
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
